f71697d7928b237c3046e699091c74a3b3b65bb3
Matheus <85845749+rmathr@users.noreply.github.com>
2023-03-23 16:04:58 -0400

    Create home and contact pages generation logic

--- src/contact.js
+++ src/contact.js
@@ -1,3 +1,25 @@
 import interactDOM from "./interact-dom";
 
+const generateContactPage = function () {
+    const mainContent = interactDOM().hookDOMelement('mainContent')
+    const mainContactContent = interactDOM().createElementWithClassAndId('main', 'main-contact-content', 'mainContactContent')
+    const contactTitle = interactDOM().createElementWithClassAndId('p', 'contact-title', 'contactTitle')
 
+    const firstContactName = interactDOM().createElementWithClassAndId('p', 'first-contact-name', 'firstContactName')
+    const firstContactRole = interactDOM().createElementWithClassAndId('p', 'first-contact-role', 'firstContactRole')
+    const firstContactPhone = interactDOM().createElementWithClassAndId('p', 'first-contact-phone', 'firstContactPhone')
+
+    mainContactContent.appendChild(contactTitle)
+    mainContactContent.appendChild(firstContactName)
+    mainContactContent.appendChild(firstContactRole)
+    mainContactContent.appendChild(firstContactPhone)
+
+    contactTitle.textContent = 'Contact Us'
+    firstContactName.textContent = 'Oscar Stone'
+    firstContactRole.textContent = 'Chef'
+    firstContactPhone.textContent = '(478) 658 112'
+
+    mainContent.appendChild(mainContactContent)
+}
+
+export default generateContactPage
--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import interactDOM from "./interact-dom"
 import generateHomepage from "./home"
+import generateContactPage from "./contact"
 
+const generateHeader = (function(){
+    const mainHeader = interactDOM().createElementWithClassAndId('header', 'main-header', 'mainHeader')
+    
+    const homeButton = interactDOM().createElementWithClassAndId('button', 'home-page', 'homePage')
+    homeButton.textContent = 'Home'
+    const contactButton = interactDOM().createElementWithClassAndId('button', 'contact-page', 'contactPage')
+    contactButton.textContent = 'Contact'
 
+    mainHeader.appendChild(homeButton)
+    mainHeader.appendChild(contactButton)
 
+    document.body.appendChild(mainHeader)
+    return { homeButton, contactButton }
+})()
+
+generateHeader.contactButton.addEventListener('click', e => {
+         e.preventDefault()
+         generateContactPage()
+     })
+
+generateHeader.homeButton.addEventListener('click', e => {
+    e.preventDefault()
+    generateHomepage()
+})
+// const changePage = interactDOM().createElementWithClassAndId('button', 'change-page', 'changePage')
+// changePage.textContent = 'Change'
 // const mainContent = interactDOM().hookDOMelement('mainContent')
-// const image = interactDOM().createElementWithClassAndId('img', 'test-image', 'testImage')
-// image.src = '../../../imgs/manchinha.jpg'
+// mainContent.appendChild(changePage)
 
-// mainContent.appendChild(image)
 
-generateHomepage()
+// changePage.addEventListener('click', e => {
+//     e.preventDefault()
+//     generateContactPage()
+// })
+
+
+// generateHomepage()
+// generateContactPage()

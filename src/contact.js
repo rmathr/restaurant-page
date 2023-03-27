362b354f1545eb3926cfe62d537f96c96a00da14
Matheus <85845749+rmathr@users.noreply.github.com>
2023-03-27 10:13:24 -0400

    Include food creator function on menu page

--- src/contact.js
+++ src/contact.js
@@ -1,23 +1,72 @@
 import interactDOM from "./interact-dom";
 
+const contactList = []
+
+const createContact = (name, role, phone, order) => {
+    const state = {
+        name,
+        role,
+        phone,
+        order
+    }
+    return { ...state }
+}
+
+const addContact = (name, role, phone, order) => { 
+    const contact = createContact(name, role, phone, order)
+    const domElements = {
+        name: interactDOM().createElementWithClassAndId('p', `${contact.name}-${contact.order}`, `${contact.name}${contact.order}`),
+        role: interactDOM().createElementWithClassAndId('p', `${contact.role}-${contact.order}`, `${contact.role}${contact.order}`),
+        phone: interactDOM().createElementWithClassAndId('p', `${contact.name}-${contact.order}-phone`, `${contact.name}-${contact.order}Phone`)
+    }
+    contactList.push(contact)
+    return domElements
+
+}
+
 const generateContactPage = function () {
     const mainContent = interactDOM().hookDOMelement('mainContent')
     const mainContactContent = interactDOM().createElementWithClassAndId('main', 'main-contact-content', 'mainContactContent')
     const contactTitle = interactDOM().createElementWithClassAndId('p', 'contact-title', 'contactTitle')
+    
+    const containerOne = interactDOM().createElementWithClassAndId('div', 'contact-container', 'containerOne')
+    const containerTwo = interactDOM().createElementWithClassAndId('div', 'contact-container', 'containerTwo')
+    const containerThree = interactDOM().createElementWithClassAndId('div', 'contact-container', 'containerThree')
 
-    const firstContactName = interactDOM().createElementWithClassAndId('p', 'first-contact-name', 'firstContactName')
-    const firstContactRole = interactDOM().createElementWithClassAndId('p', 'first-contact-role', 'firstContactRole')
-    const firstContactPhone = interactDOM().createElementWithClassAndId('p', 'first-contact-phone', 'firstContactPhone')
-
+    const contactOne = addContact('Ellie', 'President', '(781) 214 6563', 0)
+    const contactTwo = addContact('Dina', 'Vice-President', '(781) 147 788', 1)
+    const contactThree = addContact('Joel', 'Chef', '(781) 154 4487', 2)
+    
     mainContactContent.appendChild(contactTitle)
-    mainContactContent.appendChild(firstContactName)
-    mainContactContent.appendChild(firstContactRole)
-    mainContactContent.appendChild(firstContactPhone)
+    containerOne.appendChild(contactOne.name)
+    containerOne.appendChild(contactOne.role)
+    containerOne.appendChild(contactOne.phone)
 
+    containerTwo.appendChild(contactTwo.name)
+    containerTwo.appendChild(contactTwo.role)
+    containerTwo.appendChild(contactTwo.phone)
+
+    containerThree.appendChild(contactThree.name)
+    containerThree.appendChild(contactThree.role)
+    containerThree.appendChild(contactThree.phone)
+
+
+    mainContactContent.appendChild(containerOne)
+    mainContactContent.appendChild(containerTwo)
+    mainContactContent.appendChild(containerThree)
+   
     contactTitle.textContent = 'Contact Us'
-    firstContactName.textContent = 'Oscar Stone'
-    firstContactRole.textContent = 'Chef'
-    firstContactPhone.textContent = '(478) 658 112'
+    contactOne.name.textContent = `${contactList[0].name}`
+    contactOne.role.textContent = `${contactList[0].role}`
+    contactOne.phone.textContent = `${contactList[0].phone}`
+
+    contactTwo.name.textContent = `${contactList[1].name}`
+    contactTwo.role.textContent = `${contactList[1].role}`
+    contactTwo.phone.textContent = `${contactList[1].phone}`
+
+    contactThree.name.textContent = `${contactList[2].name}`
+    contactThree.role.textContent = `${contactList[2].role}`
+    contactThree.phone.textContent = `${contactList[2].phone}`
 
     mainContent.appendChild(mainContactContent)
 }
--- src/menu.js
+++ src/menu.js
@@ -1,13 +1,91 @@
 import interactDOM from "./interact-dom";
 
+const menu = {
+    desserts: []
+}
+
+const createFood = (name, description, price, img, order) => {
+    const state = {
+        name,
+        description,
+        price,
+        img,
+        order
+    }
+    return { ...state }
+}
+
+const addFood = (name, description, price, img, order, type) => {
+    const food = createFood(name, description, price, img, order)
+
+    const domElements = {
+        name: interactDOM().createElementWithClassAndId('p', 'foods-name', `name#${food.order}`),
+        description: interactDOM().createElementWithClassAndId('p', 'foods-description', `description#${food.order}`),
+        price: interactDOM().createElementWithClassAndId('p', 'foods-price', `price#${food.order}`),
+        img: interactDOM().createElementWithClassAndId('img', 'foods-image', `image#${food.order}`)
+    }
+    menu[`${type}`].push(food)
+    return domElements
+}
+
+
+
 
 const generateMenuContent = function () {
     const mainContent = interactDOM().hookDOMelement('mainContent')
     const mainMenuContent = interactDOM().createElementWithClassAndId('main', 'main-menu-content', 'mainMenuContent')
     const menuTitle = interactDOM().createElementWithClassAndId('p', 'menu-title', 'menuTitle')
+    const dessertsContent = interactDOM().createElementWithClassAndId('div', 'dessert-content', 'dessertContent')
+    const dessertContainerOne = interactDOM().createElementWithClassAndId('div', 'dessert-container', 'dessertContainerOne')
+    const dessertContainerTwo = interactDOM().createElementWithClassAndId('div', 'dessert-container', 'dessertContainerTwo')
+
+    const dessertOne = addFood(
+    "Caramel Cheesecake", 
+    `Caramel Cheesecake combines the creamy texture of cheesecake with the rich, 
+    buttery sweetness of caramel. Every bite of Caramel Cheesecake is a heavenly experience!`, 
+    "7",
+     "../src/img/dessert1.jpg",
+    0,
+    "desserts")
+
+
+    const dessertTwo = addFood(
+    "Raspberry Cheesecake", 
+    `
+    Raspberry Cheesecake perfectly blends the tangy sweetness of raspberries with 
+    the creamy richness of cheesecake. Every slice of Raspberry Cheesecake will leave you craving for more."`, 
+    "8", 
+    "../src/img/dessert2.jpg", 
+    1, 
+    "desserts")
+
 
     mainMenuContent.appendChild(menuTitle)
     menuTitle.textContent = 'Menu'
+
+    mainMenuContent.appendChild(dessertsContent)
+    dessertsContent.appendChild(dessertContainerOne)
+    dessertsContent.appendChild(dessertContainerTwo)
+
+    dessertContainerOne.appendChild(dessertOne.name)
+    dessertContainerOne.appendChild(dessertOne.description)
+    dessertContainerOne.appendChild(dessertOne.price)
+    dessertContainerOne.appendChild(dessertOne.img)
+
+    dessertOne.name.textContent = `${menu.desserts[0].name}`
+    dessertOne.description.textContent = `${menu.desserts[0].description}`
+    dessertOne.price.textContent = `${menu.desserts[0].price}`
+    dessertOne.img.src = `${menu.desserts[0].img}`
+
+    dessertContainerTwo.appendChild(dessertTwo.name)
+    dessertContainerTwo.appendChild(dessertTwo.description)
+    dessertContainerTwo.appendChild(dessertTwo.price)
+    dessertContainerTwo.appendChild(dessertTwo.img)
+
+    dessertTwo.name.textContent = `${menu.desserts[1].name}`
+    dessertTwo.description.textContent = `${menu.desserts[1].description}`
+    dessertTwo.price.textContent = `${menu.desserts[1].price}`
+    dessertTwo.img.src = `${menu.desserts[1].img}`
 
     mainContent.appendChild(mainMenuContent)
 }
